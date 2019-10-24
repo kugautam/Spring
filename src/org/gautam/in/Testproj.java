ed525e5445a19959d6eb4f4cf6f7cf5114221203
gkumar45 <gkumar45@LIN34001291.corp.capgemini.com>
2019-10-24 08:47:55 +0530

    Junit Confi done

--- src/org/gautam/in/Testproj.java
+++ src/org/gautam/in/Testproj.java
@@ -2,17 +2,23 @@ package org.gautam.in;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.*;
 
-public class Test
+public class Testproj
 {
-
+/*
 		public static void main(String[] args)
 		{
 	       ApplicationContext context=new ClassPathXmlApplicationContext("Sample.xml");
 	       Customer customer=(Customer)context.getBean("customer");
 	       System.out.println(customer);
 		}
-
-	
+*/
+	@Test
+	public void getText() {
+		ApplicationContext context=new ClassPathXmlApplicationContext("Sample.xml");
+	       Customer customer=(Customer)context.getBean("customer");
+	       System.out.println(customer);
+	}
 
 }
